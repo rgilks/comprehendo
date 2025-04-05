9399bdc7970631b486972a356049d326ba8af2f6
rgilks <rob.gilks@gmail.com>
2025-04-05 08:11:28 +0100

    fix: update import paths in HomeContent

--- app/[lang]/HomeContent.tsx
+++ app/[lang]/HomeContent.tsx
@@ -1,8 +1,8 @@
 import { useTranslation } from 'react-i18next';
 import Link from 'next/link';
-import LanguageSelector from './LanguageSelector';
-import AuthButton from './AuthButton';
-import TextGenerator from './TextGenerator';
+import LanguageSelector from '../components/LanguageSelector';
+import AuthButton from '../components/AuthButton';
+import TextGenerator from '../components/TextGenerator';
 
 export default function HomeContent() {
   const { t } = useTranslation('common');
