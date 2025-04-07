07350d6caa1b46d1baa739d81407ae5c296a6e17
rgilks <rob.gilks@gmail.com>
2025-04-07 19:32:57 +0100

    refactor: simplify Sentry integration and remove example pages

--- instrumentation.ts
+++ instrumentation.ts
@@ -2,29 +2,10 @@ import * as Sentry from '@sentry/nextjs';
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    const { nodeProfilingIntegration } = require('@sentry/profiling-node');
-
     Sentry.init({
       dsn: 'https://031c80737e80721d1044ac76d03a0d73@o4509095721566208.ingest.us.sentry.io/4509095722156032',
-      integrations: [
-        nodeProfilingIntegration(),
-        Sentry.consoleIntegration(),
-        Sentry.httpIntegration(),
-        Sentry.onUncaughtExceptionIntegration(),
-        Sentry.onUnhandledRejectionIntegration(),
-      ],
-      tracesSampleRate: 1,
-      profilesSampleRate: 1,
-      debug: false,
       environment: process.env.NODE_ENV || 'development',
       release: process.env.npm_package_version,
-      attachStacktrace: true,
-      initialScope: {
-        tags: {
-          'app.name': 'comprehendo',
-          'app.type': 'nextjs',
-        },
-      },
     });
   }
 }
