6096a5ffdbd4e0104f4292d98a7c7e0c86a3d3dc
rgilks <robert.gilks@cambridge.org>
2025-04-16 16:09:36 +0100

    refactor: remove unused next-i18next dependency and config

--- next-i18next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es', 'fr', 'de', 'it', 'pt', 'ru', 'zh', 'ja', 'ko', 'hi', 'he', 'fil'],
-  },
-  defaultNS: 'common',
-  localePath: './public/locales',
-  reloadOnPrerender: process.env.NODE_ENV === 'development',
-};
